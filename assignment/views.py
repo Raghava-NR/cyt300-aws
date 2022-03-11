fe1912a5085b1d8016816b34418e8f4baf7ad3e6
Raghava-NR <2nraghavendra@gmail.com>
2022-03-11 18:15:16 -0500

    encrypt-decrypt file.

--- assignment/views.py
+++ assignment/views.py
@@ -7,12 +7,17 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import generics, mixins, viewsets, status
 import boto3
+from cryptography.fernet import Fernet
 from django.conf import settings
 import base64
-
-
-# Create your views here.
+import logging
 from django.http import HttpResponse
+from botocore.exceptions import ClientError
+
+
+
+NUM_BYTES_FOR_LEN = 4
+
 
 
 def index(request):
@@ -39,7 +44,13 @@ class SubmissionViewSet(viewsets.ModelViewSet):
         return client
 
     @action(detail=False, methods=["POST"])
-    def encrypt_data(self, request):
+    def encrypt_text(self, request):
+
+        """
+
+        :param request:
+        :return: API Response, encrypted text
+        """
 
         plain_text = request.data.get("text", "")
 
@@ -53,7 +64,13 @@ class SubmissionViewSet(viewsets.ModelViewSet):
                         status=status.HTTP_200_OK)
 
     @action(detail=False, methods=["POST"])
-    def decrypt_data(self, request):
+    def decrypt_text(self, request):
+
+        """
+
+        :param request:
+        :return: API Response, decrypted text (plain-text)
+        """
 
         encrypted_text = request.data.get("encrypted_text", "")
 
@@ -65,8 +82,154 @@ class SubmissionViewSet(viewsets.ModelViewSet):
         return Response({"Success": True, "text": decrypted_result["Plaintext"]},
                         status=status.HTTP_200_OK)
 
+    @classmethod
+    def create_data_key(cls, cmk_id, key_spec='AES_256'):
+        """Generate a data key to use when encrypting and decrypting data
+
+        :param cmk_id: KMS CMK ID or ARN under which to generate and encrypt the
+        data key.
+        :param key_spec: Length of the data encryption key. Supported values:
+            'AES_128': Generate a 128-bit symmetric key
+            'AES_256': Generate a 256-bit symmetric key
+        :return Tuple(EncryptedDataKey, PlaintextDataKey) where:
+            EncryptedDataKey: Encrypted CiphertextBlob data key as binary string
+            PlaintextDataKey: Plaintext base64-encoded data key as binary string
+        :return Tuple(None, None) if error
+        """
+
+        # Create data key
+        kms_client = cls.aws_kms_client()
+        try:
+            response = kms_client.generate_data_key(KeyId=cmk_id, KeySpec=key_spec)
+        except ClientError as e:
+            logging.error(e)
+            return None, None
+
+        # Return the encrypted and plaintext data key
+        return response['CiphertextBlob'], base64.b64encode(response['Plaintext'])
 
 
+    @classmethod
+    def decrypt_data_key(cls, data_key_encrypted):
+
+        """Decrypt an encrypted data key
+
+        :param data_key_encrypted: Encrypted ciphertext data key.
+        :return Plaintext base64-encoded binary data key as binary string
+        :return None if error
+        """
+
+        # Decrypt the data key
+        kms_client = cls.aws_kms_client()
+
+        response = kms_client.decrypt(CiphertextBlob=data_key_encrypted)
+
+        # Return plaintext base64-encoded binary data key
+        return base64.b64encode((response['Plaintext']))
+
+
+    def encrypt_file(self, filename, cmk_id):
+
+        """Encrypt a file using an AWS KMS CMK
+
+        A data key is generated and associated with the CMK.
+        The encrypted data key is saved with the encrypted file. This enables the
+        file to be decrypted at any time in the future and by any program that
+        has the credentials to decrypt the data key.
+        The encrypted file is saved to <filename>.encrypted
+        Limitation: The contents of filename must fit in memory.
+
+        :param filename: File to encrypt
+        :param cmk_id: AWS KMS CMK ID or ARN
+        :return: True if file was encrypted. Otherwise, False.
+        """
+
+        # Read the entire file into memory
+        try:
+            with open(filename, 'rb') as file:
+                file_contents = file.read()
+        except IOError as e:
+            logging.error(e)
+            return False
+
+        # Generate a data key associated with the CMK
+        # The data key is used to encrypt the file. Each file can use its own
+        # data key or data keys can be shared among files.
+        # Specify either the CMK ID or ARN
+        data_key_encrypted, data_key_plaintext = self.create_data_key(cmk_id)
+        if data_key_encrypted is None:
+            return False
+        logging.info('Created new AWS KMS data key')
+
+        # Encrypt the file
+        f = Fernet(data_key_plaintext)
+        file_contents_encrypted = f.encrypt(file_contents)
+
+        # Write the encrypted data key and encrypted file contents together
+        try:
+            with open(filename + '.encrypted', 'wb') as file_encrypted:
+                file_encrypted.write(len(data_key_encrypted).to_bytes(NUM_BYTES_FOR_LEN,
+                                                                      byteorder='big'))
+                file_encrypted.write(data_key_encrypted)
+                file_encrypted.write(file_contents_encrypted)
+        except IOError as e:
+            logging.error(e)
+            return False
+
+        # For the highest security, the data_key_plaintext value should be wiped
+        # from memory. Unfortunately, this is not possible in Python. However,
+        # storing the value in a local variable makes it available for garbage
+        # collection.
+        return True
+
+    def decrypt_file(self, filename):
+        """Decrypt a file encrypted by encrypt_file()
+
+        The encrypted file is read from <filename>.encrypted
+        The decrypted file is written to <filename>.decrypted
+
+        :param filename: File to decrypt
+        :return: True if file was decrypted. Otherwise, False.
+        """
+
+        # Read the encrypted file into memory
+        try:
+            with open(filename + '.encrypted', 'rb') as file:
+                file_contents = file.read()
+        except IOError as e:
+            logging.error(e)
+            return False
+
+        # The first NUM_BYTES_FOR_LEN bytes contain the integer length of the
+        # encrypted data key.
+        # Add NUM_BYTES_FOR_LEN to get index of end of encrypted data key/start
+        # of encrypted data.
+        data_key_encrypted_len = int.from_bytes(file_contents[:NUM_BYTES_FOR_LEN],
+                                                byteorder='big') \
+                                 + NUM_BYTES_FOR_LEN
+        data_key_encrypted = file_contents[NUM_BYTES_FOR_LEN:data_key_encrypted_len]
+
+        # Decrypt the data key before using it
+        data_key_plaintext = self.decrypt_data_key(data_key_encrypted)
+        if data_key_plaintext is None:
+            return False
+
+        # Decrypt the rest of the file
+        f = Fernet(data_key_plaintext)
+        file_contents_decrypted = f.decrypt(file_contents[data_key_encrypted_len:])
+
+        # Write the decrypted file contents
+        try:
+            with open(filename + '.decrypted', 'wb') as file_decrypted:
+                file_decrypted.write(file_contents_decrypted)
+        except IOError as e:
+            logging.error(e)
+            return False
+
+        # The same security issue described at the end of encrypt_file() exists
+        # here, too, i.e., the wish to wipe the data_key_plaintext value from
+        # memory.
+        return True
 
 
 
